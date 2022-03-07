923b103d4388151edba969cb21982a6ec3369b0b
Joeri van Veen <joeri@myparcel.nl>
2022-03-07 11:23:54 +0100

    fix(build): translations download

--- private/downloadTranslations.js
+++ private/downloadTranslations.js
@@ -1,19 +1,20 @@
 const {createGettextFiles} = require('./createGettextFiles');
-const {downloadFile} = require('./downloadFile');
+const download = require('download');
 
 /**
  * Downloads translations from Google Sheets and run createGettextFiles with the received data.
  *
  * @see https://docs.google.com/spreadsheets/d/1WSx25YNJRyOZpkuJZLLY6hrNufe25SJaGH4dgX_og4I/edit#gid=0
  */
-function downloadTranslations() {
+async function downloadTranslations() {
   const documentId = '1WSx25YNJRyOZpkuJZLLY6hrNufe25SJaGH4dgX_og4I';
-  const sheetId = '1837968392';
+  const sheetId = '0';
 
-  downloadFile(
+  const data = await download(
     `https://docs.google.com/spreadsheets/d/${documentId}/gviz/tq?tqx=out:csv&gid=${sheetId}`,
-    createGettextFiles,
   );
+
+  createGettextFiles(data);
 }
 
 module.exports = {downloadTranslations};
